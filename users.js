f2967c515a0813d1bd821a2214aaef2575b64054
Albertttesla <utetileualtynaiserzhankizi@gmail.com>
2025-07-18 11:07:41 +0500

    Update users.js

---
+++ users.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  passwordHash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
+
+
