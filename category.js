ac63fb5915b5f5a4ffa7b48d7cd57d297ffefd95
Albertttesla <utetileualtynaiserzhankizi@gmail.com>
2025-07-18 11:04:53 +0500

    Update category.js

---
+++ category.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Category = sequelize.define('Category', {
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
+
+module.exports = Category;
+
